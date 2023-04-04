bd49fea7e7c02be165e3f8d8cd366a5ac5fbb3c3
Rafael Lourenco <rafael.lourenco@sltlabs.com.br>
2023-04-04 17:23:59 -0300

    404 com link vermelho

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import Title from '../components/title/title';
-
+import utils from '../styles/utils.module.scss';
 export default function NotFound() {
   return <>
   <Title>Página não encontrada</Title>
+  <div style={{
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    height: '100% '
+  }}>
     <h1>404 - Page Not Found</h1>
-        <p>vish, n tem nada nesse link não</p>
-    <Link href="/">
-        Go back home
-    </Link>
+        <p>Oh, nej! Parece que nesse link não tem nada :(</p>
+
+    <Link href="/" className={utils.links}>Clique aqui para voltar para o menu principal e continuar explorando!</Link>
+  </div>
   </>
 }
